788af3203bca1d4e4b1412e0549887b928bb739a
deep-sarkar <deepsarkar96@gmail.com>
2020-10-14 21:58:39 +0530

    [Deep]Add.added function to ewturn all prime number in range

--- logical/prime_number.py
+++ logical/prime_number.py
@@ -12,7 +12,22 @@ def isPrime(num):
     except TypeError:
         return "Enter numerical value"
 
-print(isPrime(97))
-print(isPrime(71))
-print(isPrime(10971))
-print(isPrime("97"))
+# print(isPrime(97))
+# print(isPrime(71))
+# print(isPrime(10971))
+# print(isPrime("97"))
+
+def primeInRange(lower_range, upper_range):
+    all_prime = []
+    try:
+        for number in range(lower_range, upper_range+1):
+            prime = isPrime(number)
+            if prime:
+                all_prime.append(number)
+        return all_prime
+    except TypeError:
+        return "Enter numerical value"
+
+
+all_prime = primeInRange(100, 200)
+print(all_prime)
